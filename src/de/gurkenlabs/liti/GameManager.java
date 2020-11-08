36c9adaa99a37db474060a3d808def27a02677a5
nightm4re94 <matthias@gurkenlabs.de>
2020-11-09 00:16:49 +0100

    Adjust the tileset and map dimensions.

--- src/de/gurkenlabs/liti/GameManager.java
+++ src/de/gurkenlabs/liti/GameManager.java
@@ -3,8 +3,6 @@ package de.gurkenlabs.liti;
 import de.gurkenlabs.liti.entities.Warrior;
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.environment.CreatureMapObjectLoader;
-import de.gurkenlabs.litiengine.graphics.PositionLockCamera;
-import de.gurkenlabs.litiengine.input.Input;
 import de.gurkenlabs.litiengine.input.KeyboardEntityController;
 import de.gurkenlabs.litiengine.physics.IMovementController;
 
@@ -16,15 +14,14 @@ public final class GameManager {
     CreatureMapObjectLoader.registerCustomCreatureType(Warrior.class);
 
     Game.world().onLoaded(e -> {
-      Game.graphics().setBaseRenderScale(4.001f);
+      Game.graphics().setBaseRenderScale(2.5f);
 
       // INIT WARRIOR WITH CONTROLLER AND CAMERA FOR TESTING
       Warrior warrior = Game.world().environment().get(Warrior.class, "warrior-test");
       if (warrior != null) {
         IMovementController controller = new KeyboardEntityController<>(warrior);
         warrior.setController(IMovementController.class, controller);
-
-        Game.world().setCamera(new PositionLockCamera(warrior));
+        Game.world().camera().setFocus(Game.world().environment().getCenter());
       }
     });
   }
