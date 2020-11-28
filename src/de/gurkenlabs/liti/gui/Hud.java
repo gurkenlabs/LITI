8650758c52da2bf86e4e24d7fede8a683fa1c214
Steffen Wilke <mail@steffen-wilke.com>
2020-11-28 01:15:59 +0100

    Add hud and make warrior slower.

--- src/de/gurkenlabs/liti/gui/Hud.java
+++ src/de/gurkenlabs/liti/gui/Hud.java
@@ -1,17 +1,50 @@
 package de.gurkenlabs.liti.gui;
 
+import de.gurkenlabs.liti.entities.Player;
+import de.gurkenlabs.liti.entities.Players;
 import de.gurkenlabs.litiengine.Direction;
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.graphics.IRenderable;
+import de.gurkenlabs.litiengine.gui.GuiComponent;
 
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 //TODO: dispatching delay
-public final class Hud {
+public final class Hud extends GuiComponent implements IRenderable {
   private static final int UI_INPUT_DELAY = 100;
   private static Map<Integer, Long> lastInputs = new ConcurrentHashMap<>();
 
-  private Hud() {
+  Hud() {
+    super(0, 0);
+  }
+
+  @Override
+  public void render(Graphics2D g) {
+    super.render(g);
+
+    for (Player player : Players.getAll().values()) {
+      final double width = 16;
+      final double height = 2;
+      double x = player.getX() - (width - player.getWidth()) / 2.0;
+      double y = player.getY() - height * 2;
+      RoundRectangle2D rect = new RoundRectangle2D.Double(x, y, width, height, 1.5, 1.5);
+
+      final double currentWidth = width * (player.getHitPoints().get() / (double) player.getHitPoints().getMax());
+      RoundRectangle2D actualRect = new RoundRectangle2D.Double(x, y, currentWidth, height, 1.5, 1.5);
+
+      g.setColor(new Color(40, 42, 43, 200));
+      Game.graphics().renderShape(g, rect);
+
+      if (player.getHitPoints().get() < 33) {
+        g.setColor(Color.RED);
+      } else {
+        g.setColor(new Color(225, 109, 115));
+      }
+      Game.graphics().renderShape(g, actualRect);
+    }
   }
 
   public static void cancel(int player) {
