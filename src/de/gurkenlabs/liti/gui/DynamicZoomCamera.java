aa3f608d3a33da4bf9fb7027e475bca9dc945f58
Steffen Wilke <mail@steffen-wilke.com>
2021-01-04 00:52:32 +0100

    Clamp camera to map

--- src/de/gurkenlabs/liti/gui/DynamicZoomCamera.java
+++ src/de/gurkenlabs/liti/gui/DynamicZoomCamera.java
@@ -13,11 +13,12 @@ import de.gurkenlabs.litiengine.util.MathUtilities;
 
 public class DynamicZoomCamera extends Camera {
 
-  private static float minZoom = .3f;
+  private static float minZoom = .71f;
   private static float maxZoom = 1.4f;
   private static short padding = 20;
 
   public DynamicZoomCamera() {
+    this.setClampToMap(true);
     this.setFocus(Game.world().environment().getCenter());
   }
 
@@ -54,6 +55,6 @@ public class DynamicZoomCamera extends Camera {
     this.setFocus(bounds.getCenterX(), bounds.getCenterY());
     double rel = Math.min(Game.world().environment().getMap().getSizeInPixels().getWidth() / bounds.getWidth() / 2, Game.world().environment().getMap().getSizeInPixels().getHeight() / bounds.getHeight() / 2);
     float targetZoom = MathUtilities.clamp((float) rel, minZoom, maxZoom);
-    this.setZoom(targetZoom, 0);
+    this.setZoom(targetZoom, 100);
   }
 }
