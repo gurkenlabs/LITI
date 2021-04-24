1fff196c21b269b36ee4e60791f0e962648ff16d
Steffen <mail@steffen-wilke.de>
2021-04-25 01:35:29 +0200

    Highlight players behind props.

--- src/de/gurkenlabs/liti/gui/IngameScreen.java
+++ src/de/gurkenlabs/liti/gui/IngameScreen.java
@@ -1,14 +1,30 @@
 package de.gurkenlabs.liti.gui;
 
+import de.gurkenlabs.liti.entities.Player;
+import de.gurkenlabs.liti.entities.Players;
 import de.gurkenlabs.litiengine.Direction;
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.entities.EntityYComparator;
+import de.gurkenlabs.litiengine.entities.Prop;
+import de.gurkenlabs.litiengine.environment.Environment;
+import de.gurkenlabs.litiengine.graphics.ImageRenderer;
+import de.gurkenlabs.litiengine.graphics.RenderType;
+import de.gurkenlabs.litiengine.graphics.ShapeRenderer;
+import de.gurkenlabs.litiengine.graphics.animation.IEntityAnimationController;
+import de.gurkenlabs.litiengine.util.Imaging;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
 public class IngameScreen extends LitiScreen {
   private Hud hud;
 
   private static IngameScreen instance;
+  private final EntityYComparator entityComparator = new EntityYComparator();
+
   private IngameScreen() {
     super("INGAME");
   }
@@ -28,7 +44,53 @@ public class IngameScreen extends LitiScreen {
   @Override
   public void prepare() {
     super.prepare();
-    Game.world().loadEnvironment("plateau2");
+    Environment plateau = Game.world().loadEnvironment("plateau2");
+    plateau.onRendered(RenderType.OVERLAY, (g, t) -> {
+      for(Player player : Players.getAll()){
+
+        if(!Game.graphics().canRender(player)){
+          continue;
+        }
+
+        for(Prop prop : Game.world().environment().getProps()){
+          if(prop.getBoundingBox().intersects(player.getBoundingBox())
+                  && (prop.getCenter().distance(player.getCenter()) < 10
+                      || prop.getBoundingBox().contains(player.getCollisionBox().getBounds2D()))
+                  && this.entityComparator.compare(prop, player) > 0){
+            final IEntityAnimationController<?> animationController = player.animations();
+              final BufferedImage img = animationController.getCurrentImage();
+              if (img != null) {
+                if (animationController.isAutoScaling()) {
+                  final double ratioX = player.getWidth() / img.getWidth();
+                  final double ratioY = player.getHeight() / img.getHeight();
+                  ImageRenderer.renderScaled(g, img, Game.world().camera().getViewportLocation(player.getLocation()), ratioX, ratioY);
+                } else {
+                  // center the image relative to the entity dimensions -> the pivot point for rendering is the center of the entity
+                  double deltaX = (player.getWidth() - img.getWidth()) / 2.0;
+                  double deltaY = (player.getHeight() - img.getHeight()) / 2.0;
+
+                  final AffineTransform transform = animationController.getAffineTransform();
+                  if (transform != null) {
+                    // center the scaled image relative to the desired render location if the transform provides a scaling element
+                    deltaX += (img.getWidth() - (img.getWidth() * transform.getScaleX())) / 2.0;
+                    deltaY += (img.getHeight() - (img.getHeight() * transform.getScaleY())) / 2.0;
+                  }
+
+                  Point2D renderLocation = Game.world().camera().getViewportLocation(player.getX() + deltaX, player.getY() + deltaY);
+
+                  Composite comp = g.getComposite();
+                  g.setComposite(AlphaComposite.getInstance(AlphaComposite.DST_OUT, 0.5f));
+                  ImageRenderer.renderTransformed(g, img, renderLocation.getX(), renderLocation.getY(), transform);
+
+                  g.setComposite(comp);
+                }
+              }
+
+            break;
+          }
+        }
+      }
+    });
   }
 
   @Override
