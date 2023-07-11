afe1b9f638d82352075105c584c62032926ca7f7
Matthias Wilke <matthias@gurkenlabs.de>
2023-07-11 23:15:28 +0200

    Update Gradle wrapper

--- src/de/gurkenlabs/liti/constants/LitiSounds.java
+++ src/de/gurkenlabs/liti/constants/LitiSounds.java
@@ -3,8 +3,6 @@ package de.gurkenlabs.liti.constants;
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.sound.Sound;
 
-import java.awt.*;
-
 public final class LitiSounds {
   public static final Sound UI_DIRECTION_UP = Resources.sounds().get("UI_direction_up");
   public static final Sound UI_DIRECTION_DOWN = Resources.sounds().get("UI_direction_down");
